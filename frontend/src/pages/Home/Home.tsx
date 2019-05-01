936594d598eed55c95648c5887929711cb73cef2
lixxday <corentin.normant@gmail.com>
2019-05-01 22:58:52 +0200

    Composant Pokemon avec props id + nom

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import Pokemon from 'components/Pokemon';
 import Style from './Home.style';
 
 class Home extends React.Component {
@@ -9,21 +10,9 @@ class Home extends React.Component {
 
     return (
       <Style.Intro>
-        <div>Bienvenue sur ton futur pokédex !</div>
-        <div>
-          Tu vas pouvoir apprendre tout ce qu'il faut sur React, Redux et Symfony, et attraper des
-          pokemons !
-        </div>
-        <div>Commence par créer ton premier pokemon: {pokemon}</div>
-        <br />
-        <div>
-          <div>{pokemon}</div>
-          <div>No {num}</div>
-          <img
-            src="https://raw.githubuser.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-            alt="Carapuce_img"
-          />
-        </div>
+        <Pokemon id={7} name="Carapuce" />
+        <Pokemon id={8} name="Carabafe" />
+        <Pokemon id={9} name="Tortank" />
       </Style.Intro>
     );
   }
