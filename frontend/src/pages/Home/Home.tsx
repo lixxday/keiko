66b10d21e4e32fb74106d741e172da0ceba5281d
lixxday <corentin.normant@gmail.com>
2019-05-07 23:54:18 +0200

    api call Salameche

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -13,28 +13,38 @@ interface State {
 }
 
 class Home extends React.Component<Props, State> {
-  /*
   constructor(props: any) {
     super(props);
     this.state = {
       pokemons: [],
     };
   }
-  */
 
   componentDidMount() {
     const pokemonList = makeGetRequest('/pokemon')
       .then(response => {
-        console.log(response.body);
+        this.setState({
+          pokemons: response.body,
+        });
+        return response.body;
       })
       .catch(err => console.log(err.message));
   }
 
   render(): React.ReactNode {
-    const salameche = this.state.pokemons[0];
+    const salameche = this.state.pokemons[3];
+
+    // Probablement pas le meilleur moyen d'attendre le chargement du component
+    if (salameche === undefined) {
+      return <Style.Intro />;
+    }
+
     return (
       <Style.Intro>
-        <Pokemon id={4} name="Salamèche" />
+        <Pokemon id={salameche.id} name={salameche.name} />
+        <Pokemon id={7} name="Carapuce" />
+        <Pokemon id={8} name="Carabaffe" />
+        <Pokemon id={9} name="Tortank" />
       </Style.Intro>
     );
   }
