52d60c3e0c20ce68e0b4ff8a920a5086b2b4edaf
lixxday <corentin.normant@gmail.com>
2019-05-07 22:41:33 +0200

    first API call. Not working

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,15 +1,40 @@
 import * as React from 'react';
 
+import { makeGetRequest } from 'services/networking/request';
 import Pokemon from 'components/Pokemon';
 import Style from './Home.style';
 
-class Home extends React.Component {
+interface Props {}
+interface State {
+  pokemons: {
+    id: number;
+    name: string;
+  }[];
+}
+
+class Home extends React.Component<Props, State> {
+  /*
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      pokemons: [],
+    };
+  }
+  */
+
+  componentDidMount() {
+    const pokemonList = makeGetRequest('/pokemon')
+      .then(response => {
+        console.log(response.body);
+      })
+      .catch(err => console.log(err.message));
+  }
+
   render(): React.ReactNode {
+    const salameche = this.state.pokemons[0];
     return (
       <Style.Intro>
-        <Pokemon id={7} name="Carapuce" />
-        <Pokemon id={8} name="Carabafe" />
-        <Pokemon id={9} name="Tortank" />
+        <Pokemon id={4} name="Salamèche" />
       </Style.Intro>
     );
   }
