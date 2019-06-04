5a7b48f8f0481d91a0120d3b192011402507202a
lixxday <corentin.normant@gmail.com>
2019-06-04 18:26:31 +0200

    Design cible for the Pokedex
    Up to date with master

--- frontend/src/components/Pokemon/Pokemon.style.tsx
+++ frontend/src/components/Pokemon/Pokemon.style.tsx
@@ -1,16 +1,8 @@
 import styled from 'styled-components';
-import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Pokemon';
-    src: 'PokemonGB.ttf';
-  }
-`;
 export default {
   Pokemon: styled.div`
     display: inline-block;
-    font-family: 'Pokemon';
     text-align: center;
     border: 5px double black;
     padding: 5px 0 10px 0;
