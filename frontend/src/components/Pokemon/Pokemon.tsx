311b06b383440ca9c309d4286b322853d9f2840d
lixxday <corentin.normant@gmail.com>
2019-06-06 16:44:59 +0200

    Component Pokemon is now a functional component

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -8,23 +8,19 @@ interface Props {
   weight: number;
 }
 
-class Pokemon extends React.Component<Props> {
-  render(): React.ReactNode {
-    return (
-      <Style.Pokemon>
-        <div>{this.props.name}</div>
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            this.props.id
-          }.png`}
-          alt={`Image pour ${this.props.name}`}
-        />
-        <div>Id:{this.props.id}</div>
-        <div>Weight:{this.props.weight / 10} kg</div>
-        <div>Height:{this.props.height * 10} cm</div>
-      </Style.Pokemon>
-    );
-  }
-}
+const Pokemon = (props: Props) => (
+  <Style.Pokemon>
+    <div>{props.name}</div>
+    <img
+      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+        props.id
+      }.png`}
+      alt={`Image pour ${props.name}`}
+    />
+    <div>Id:{props.id}</div>
+    <div>Weight:{props.weight / 10} kg</div>
+    <div>Height:{props.height * 10} cm</div>
+  </Style.Pokemon>
+);
 
 export default Pokemon;
