05823b5a9b069d7817caaf79d5831e157028b3c8
Lixxday <corentin.normant@gmail.com>
2019-06-07 17:16:32 +0200

    Annotated route for /pokemon

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class PokemonController
 {
+    /**
+     * @Route("/pokemon", name="pokemon")
+     */
     public function helloWorld()
     {
         return new Response(
