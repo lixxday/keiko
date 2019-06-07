4eefb614d96eaeb901d1bd09c19b957114636008
Lixxday <corentin.normant@gmail.com>
2019-06-07 18:22:44 +0200

    Pokemon JSON - ID missing

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -4,17 +4,35 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Pokemon;
+
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 
 class PokemonController
 {
+
     /**
      * @Route("/pokemon", name="pokemon")
      */
-    public function helloWorld()
+    public function pokemon()
     {
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $pokemon = new Pokemon();
+        $pokemon -> setName("bulbasaur");
+        $pokemon -> setWeight(67);
+        $pokemon -> setHeight(100);
+
+        $jsonPokemon = $serializer->serialize($pokemon, 'json');
+
         return new Response(
-            '<html><body>Hello World!</body></html>'
+            '<html><body>'.$jsonPokemon.'</body></html>'
         );
     }
 }
